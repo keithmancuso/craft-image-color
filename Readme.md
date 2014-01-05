f2a28ad85ad0c3d36f9eab4f6d827eeda0fe688c
Keith Mancuso <keithmancuso@gmail.com>
2014-01-05 18:07:39 -0500

    Commenting out field type settings for now

---
+++ Readme.md
@@ -0,0 +1,39 @@
+Craft-Image-Color
+=================
+
+Simple Image Color filter for Craft CMS.
+
+Grabs the most prominate colors from any image asset using a twig filter on an assets object
+
+
+Usage
+------
+
+
+```
+<div style="background-color: url({{ entry.image[0]|imageColor }})">
+
+	<img src="{{ entry.image[0].url }}"/>
+</div>
+
+```
+Grabs the second most prominate color (0 index).  Currently saves up to 5 colors per image
+
+```
+<div style="background-color: url({{ entry.image[0]|imageColor(1) }})">
+
+	<img src="{{ entry.image[0].url }}"/>
+</div>
+
+```
+
+
+
+To-do
+------
+
+Working on a number of new features
+
+* Settings screen to set number of colors saved
+* Assets fieldtype to show dropdown of colors and letting you select which one to use on an image by image basis
+* Better error checking and defaults.
--- imagecolor/fieldtypes/ImageColorFieldType.php
+++ imagecolor/fieldtypes/ImageColorFieldType.php
@@ -1,4 +1,5 @@
 <?php
+/*
 namespace Craft;
 
 class ImageColorFieldType extends BaseFieldType
@@ -13,77 +14,13 @@ class ImageColorFieldType extends BaseFieldType
 		return Craft::t('Image Color Options');
 	}
 
-	/*
-protected function defineSettings()
-	{
-		return array(
-			'options' => array(AttributeType::Mixed, 'default' => array()),
-			'newPersistent' => AttributeType::Bool,
-		);
-	}
-
-	public function getSettingsHtml()
-	{
-		$html = parent::getSettingsHtml();
-		$html .= craft()->templates->renderMacro('_includes/forms.html', 'checkboxField', array(
-			array(
-				'label' => Craft::t('Make new options persistent?'),
-				'id' => 'newPersistent',
-				'name' => 'newPersistent',
-				'checked' => $this->getSettings()->newPersistent,
-			)
-		));
-		return $html;
-	}
-*/
-
 	public function getInputHtml($name, $value)
 	{
 	
 		$color = craft()->imageColor->getColor($this->element);
 		
 	
-		
-		//$color = 'red';
-		
-		//$options = array();
-		
-		
-		/*
-
-		$options = $this->getOptions();
-
-		// If this is a new entry, look for a default option
-		if ($this->isFresh())
-		{
-			foreach ($options as $option)
-			{
-				if (!empty($option['default']))
-				{
-					$value = $option['value'];
-					break;
-				}
-			}
-		}
-
-		$existing = false;
-		foreach ($options as $option)
-		{
-			if ($option['value'] == $value)
-			{
-				$existing = true;
-				break;
-			}
-		}
-		if (!$existing)
-		{
-			$options[] = array(
-				'label'   => $value,
-				'value'   => $value,
-				'default' => false,
-			);
-		}
-*/
+	
 
 		return craft()->templates->render('imagecolor/imagecolor', array(
 			
@@ -93,35 +30,9 @@ protected function defineSettings()
 
 	public function prepValueFromPost($value)
 	{
-		if ($this->getSettings()->newPersistent)
-		{
-			$options = $this->getOptions();
-
-			$existing = false;
-			foreach ($options as $option)
-			{
-				if ($option['value'] == $value)
-				{
-					$existing = true;
-					break;
-				}
-			}
-			if (!$existing)
-			{
-				$options[] = array(
-					'label'   => $value,
-					'value'   => $value,
-					'default' => false,
-				);
-			}
-
-			// Make new option persistent
-			$settings = $this->model->settings;
-			$settings['options'] = $options;
-			$this->model->settings = $settings;
-			craft()->fields->saveField($this->model);
-		}
+		
 
 		return $value;
 	}
 }
+*/
